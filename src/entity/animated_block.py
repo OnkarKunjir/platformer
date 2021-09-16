58854e8d5fea548fb950bfddc0a5643f26cc5c0c
OnkarKunjir <onkarkunjir8@gmail.com>
2021-09-16 18:55:12 +0530

    Switching from python to C using opengl.

--- src/entity/animated_block.py
+++
@@ -1,7 +0,0 @@
-from src.entity.animated_entity import AnimatedEntity
-
-
-class AnimatedBlock(AnimatedEntity):
-    def __init__(self, x, y, width, height, block_type, n_frames):
-        super().__init__(x, y, width, height, n_frames)
-        self.block_type = block_type
